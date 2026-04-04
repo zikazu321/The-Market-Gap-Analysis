61bd67175f81507fd2165bd718bc8b3dee81e2ec
Zabdiel Addo <kingdomstars0000@gmail.com>
2026-04-04 20:13:46 +0000

    chore

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ Email: zabdiel.addo@ashesi.edu.gh
 Version: 1.0.0
 Brief: <<brief>>
 -----
-Last Modified: Saturday, 4th April 2026 8:12:01 PM
+Last Modified: Saturday, 4th April 2026 8:13:37 PM
 Modified By: Zabdiel Addo
 -----
 Copyright ©2026 Zabdiel Addo
@@ -346,8 +346,6 @@ AXIS_STYLE = dict(
     gridwidth=1,
     linecolor="#94A3B8",
     linewidth=1.4,
-    tickfont=dict(size=13, color="#111827"),
-    title_font=dict(size=14, color="#111827"),
     tickcolor="#94A3B8",
 )
 
